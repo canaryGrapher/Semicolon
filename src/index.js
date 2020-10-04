9e69e7d9c6b26b8e5560e641ad4df1b4451fb2ca
Yash Aryan <yash.aryan1@learner.manipal.edu>
2020-10-04 18:15:11 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -32,7 +32,6 @@ class App extends HTMLElement {
 
   connectedCallback() {
     this.renderBasicElements();
-    console.log(window.location.hash);
     if (window.location.hash === '#home' || window.location.hash === '') {
       // if (window.location.hash === '#home') {
       this.state.currentPage = 'home';
