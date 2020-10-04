d8f3311e63665ba8af509ff06e8ae7dfa84f8859
Yash Aryan <yash.aryan1@learner.manipal.edu>
2020-10-04 20:10:20 +0530

    update json

--- src/data/game_catalogue.js
+++ src/data/game_catalogue.js
@@ -1,50 +1,143 @@
 export const gameCatalogue = [
   {
+    'name': 'The Knight',
+    'cost': '4',
+    'image':
+      'https://pmcvariety.files.wordpress.com/2019/02/super-bowl-knight.jpg',
+    'about':
+      'Danger has fallen upon Acropolis and The Knight must save all.'
+  },
+  {
+    'name': 'D-Day',
+    'cost': '2.5',
+    'image':
+      'https://apicms.thestar.com.my/uploads/images/2020/07/15/758160.jpg',
+    'about':
+      'D-Day is an augmented reality game with zombies trying to bring humanity to an end.'
+  },
+  {
+    'name': 'Kyoryu',
+    'cost': '3',
+    'image':
+      'https://thetelltalemind.files.wordpress.com/2015/12/one-million-years-b-c-30.jpg?w=829',
+    'about':
+      'Kyoryu is a Japanese augmented reality game with mystical dinosaurs hell bend on destruction and you must save the world.'
+  },
+  {
+    'name': 'Deep Sea',
+    'cost': '0.9',
+    'image':
+      'https://imengine.prod.srp.navigacloud.com/?uuid=F8EEC71D-EA47-4B9D-83AA-C7585CCC7EC8&type=primary&q=72&width=1024',
+    'about':
+      'The secrets of underwater world cant contain themselves anymore, play to discover them.'
+  },
+  {
+    'name': 'Gangsters of Vegas',
+    'cost': '4.9',
+    'image':
+      'https://i.pinimg.com/originals/c4/e8/d6/c4e8d65ccebfd8cef0e3a6df91d0b1b5.jpg',
+    'about':
+      'Five years ago Carl Johnson escaped from the pressures of life in Las Vegas... a city tearing itself apart with gang trouble, drugs and corruption. Where filmstars and millionaires do their best to avoid the dealers and gangbangers.'
+  },
+  {
+    'name': 'Old Town',
+    'cost': '2.0',
+    'image':
+      'https://i.ytimg.com/vi/uRIaCTEjTm4/hqdefault.jpg',
+    'about':
+      'The cowboys of Dallas must save their vity from drugs and bandits.'
+  },
+    {
+    'name': 'Street Combat',
+    'cost': '0.8',
+    'image':
+      'https://images.pushsquare.com/aa5764f1b09ad/best-ps4-fighting-games-playstation-4-1.original.jpg',
+    'about':
+      'Street Combat is a fighting game developed by Lolo and Pimps and published by Capcom for the PlayStation 4 and Microsoft Windows in 2016.'
+  },
+  {
+    'name': 'Immortal',
+    'cost': '1.9',
+    'image':
+      'https://i.insider.com/5cc0fcd74a16754ef749f365?width=600&format=jpeg&auto=webp',
+    'about': 'Immortal is a Japanese street fight video game developed in 2013.'
+  },
+  {
+    'name': 'Sensei',
+    'cost': '0.9',
+    'image':
+      'https://previews.123rf.com/images/jaxper/jaxper1903/jaxper190300007/133058953-samurai-japanese-warrior-in-armor-ready-for-battle.jpg',
+    'about':
+      'Sensei is a combat video game with various fighting levels having stronger contestants with each level.'
+  },
+  {
+    'name': 'Hangaki Gozen',
+    'cost': '1.8',
+    'image':
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtoSVksQHFH9UVDlY5PCamSpWgeLA-lF4JsQ&usqp=CAU',
+    'about': 'Hangaku Gozen is a female warrior game based on the Greek mythology'
+  },
+  {
+    'name': 'Gangs of Nevada',
+    'cost': '3',
+    'image': 'https://i.ytimg.com/vi/YhYctIBKFbM/hqdefault.jpg',
+    'about':
+      'Gangs of Nevada is an American comic based video game with inspiration from American gang history.'
+  },
+  {
+    'name': 'The Third Eye',
+    'cost': '8',
+    'image':
+      'https://i.pinimg.com/originals/5c/1a/47/5c1a47b4697581867bcdce045731f6f9.jpg',
+    'about':
+      'The Third Eye is a video game based on the Indian mythology of Shiva'
+  },
+    {
     'name': 'Quake',
     'cost': '0.9',
     'image':
-      'https://data3.origin.com/asset/content/dam/originx/web/app/programs/About/aboutorigin_3840x2160_battlefield1.jpg/27051ac9-d3c0-49e3-9979-3dc1058a69f5/original.jpg',
+      'https://img.cinemablend.com/filter:scale/cb/d/2/0/1/1/4/d201145708819b3bbcdad34576b226049a8fd39c00de9b105ddc668d24ceba34.jpg?mw=600',
     'about':
-      'lorem ipsum doloanfk jhdsfjh bjs hlaslf halkSG GJHagfg kjHsz zg kjdgszdgjhsz shg lzsjgjhsg kjsj gsgZGZDSgdszlvb sVZ ss HSFk dfzb'
+      'It is a virtual reality first-person shooter developed and published by Valve. Between the events of Half-Life and Half-Life 2, players control Alyx Vance on a mission to seize a superweapon belonging to the alien'
   },
   {
-    'name': 'fasfcg',
+    'name': 'Beat',
     'cost': '1.9',
     'image':
-      'https://data3.origin.com/asset/content/dam/originx/web/app/programs/About/aboutorigin_3840x2160_battlefield1.jpg/27051ac9-d3c0-49e3-9979-3dc1058a69f5/original.jpg',
+      'https://www.roadtovr.com/wp-content/uploads/2018/05/ragesaq-vr-beat-saber.jpg',
     'about':
-      'lorem ipsum doloanfk jhdsfjh bjs hlaslf halkSG GJHagfg kjHsz zg kjdgszdgjhsz shg lzsjgjhsg kjsj gsgZGZDSgdszlvb sVZ ss HSFk dfzb'
+      'The game takes place in a surrealistic neon noir environment and features the player slicing blocks representing musical beats with a pair of contrasting colored sabers'
   },
   {
-    'name': 'SdsGzsdg',
-    'cost': '0.9',
+    'name': 'Arizona Alyx',
+    'cost': '1.2',
     'image':
-      'https://data3.origin.com/asset/content/dam/originx/web/app/programs/About/aboutorigin_3840x2160_battlefield1.jpg/27051ac9-d3c0-49e3-9979-3dc1058a69f5/original.jpg',
+      'https://www.mercurynews.com/wp-content/uploads/2016/08/20150204_123429_CCT-VIDGAME-0206-01.jpg',
     'about':
-      'lorem ipsum doloanfk jhdsfjh bjs hlaslf halkSG GJHagfg kjHsz zg kjdgszdgjhsz shg lzsjgjhsg kjsj gsgZGZDSgdszlvb sVZ ss HSFk dfzb'
+      'Arizona Alyx is a zombie survival first-person shooter video game released on December 6, 2016.'
   },
   {
-    'name': 'sdzgfdhgag',
-    'cost': '0.9',
+    'name': 'Valkyrie',
+    'cost': '2.5',
     'image':
-      'https://data3.origin.com/asset/content/dam/originx/web/app/programs/About/aboutorigin_3840x2160_battlefield1.jpg/27051ac9-d3c0-49e3-9979-3dc1058a69f5/original.jpg',
+      'https://cdn.wccftech.com/wp-content/uploads/2017/09/eve_valkyrie_warzone_art.jpg',
     'about':
-      'lorem ipsum doloanfk jhdsfjh bjs hlaslf halkSG GJHagfg kjHsz zg kjdgszdgjhsz shg lzsjgjhsg kjsj gsgZGZDSgdszlvb sVZ ss HSFk dfzb'
+      'Valkyrie is a multiplayer dogfighting shooter game set in the Eve Online universe that is designed to use virtual reality headset technology.'
   },
   {
-    'name': 'gaszsgvs',
-    'cost': '0.9',
+    'name': "Skyfall: No Man's Planet",
+    'cost': '1.3',
     'image':
-      'https://data3.origin.com/asset/content/dam/originx/web/app/programs/About/aboutorigin_3840x2160_battlefield1.jpg/27051ac9-d3c0-49e3-9979-3dc1058a69f5/original.jpg',
+      'https://zombiesinmyblog.com/wp-content/uploads/2016/03/Star-Wars-Universe-Poster.jpg',
     'about':
-      'lorem ipsum doloanfk jhdsfjh bjs hlaslf halkSG GJHagfg kjHsz zg kjdgszdgjhsz shg lzsjgjhsg kjsj gsgZGZDSgdszlvb sVZ ss HSFk dfzb'
+      'A virtual reality game where the earth is in danger and its residents need to find a new planet'
   },
   {
-    'name': 'vsrszdgdg',
+    'name': 'The Odyssey Danger',
     'cost': '0.9',
     'image':
-      'https://data3.origin.com/asset/content/dam/originx/web/app/programs/About/aboutorigin_3840x2160_battlefield1.jpg/27051ac9-d3c0-49e3-9979-3dc1058a69f5/original.jpg',
+      'https://cms.elitedangerous.com/frontier_image_styles/style?url=/sites/default/files/2018-11/horizons-key-art_0.jpg&width=1920&height=1080&type=binary',
     'about':
-      'lorem ipsum doloanfk jhdsfjh bjs hlaslf halkSG GJHagfg kjHsz zg kjdgszdgjhsz shg lzsjgjhsg kjsj gsgZGZDSgdszlvb sVZ ss HSFk dfzb'
+      'The Odyssey Danger is a space flight simulation game developed and published by Frontier Developments. Piloting a spaceship, the player explores a realistic 1:1 scale open-world representation of the Milky Way galaxy, with the gameplay being open-ended.'
   }
 ];
--- src/data/physical_games.js
+++ src/data/physical_games.js
@@ -1,38 +1,47 @@
 export const physicalData = [
-      {
-            "name": "Street Combat",
-            "cost": "0.8",
-            "image": "https://images.pushsquare.com/aa5764f1b09ad/best-ps4-fighting-games-playstation-4-1.original.jpg",
-            "about": "Street Combat is a fighting game developed by Lolo and Pimps and published by Capcom for the PlayStation 4 and Microsoft Windows in 2016."
-      },
-      {
-            "name": "Immortal",
-            "cost": "1.9",
-            "image": "https://i.insider.com/5cc0fcd74a16754ef749f365?width=600&format=jpeg&auto=webp",
-            "about": "Immortal is a Japanese street fight video game developed in 2013."
-      },
-      {
-            "name": "Sensei",
-            "cost": "0.9",
-            "image": "https://previews.123rf.com/images/jaxper/jaxper1903/jaxper190300007/133058953-samurai-japanese-warrior-in-armor-ready-for-battle.jpg",
-            "about": "Sensei is a combat video game with various fighting levels having stronger contestants with each level."
-      },
-      {
-            "name": "Hangaki Gozen",
-            "cost": "1.8",
-            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtoSVksQHFH9UVDlY5PCamSpWgeLA-lF4JsQ&usqp=CAU",
-            "about": "Hangaku Gozen is a female warrior game based on the Greek mythology"
-      },
-      {
-            "name": "Gangs of Nevada",
-            "cost": "3",
-            "image": "https://i.ytimg.com/vi/YhYctIBKFbM/hqdefault.jpg"
-            "about": "Gangs of Nevada is an American comic based video game with inspiration from American gang history."
-      },
-      {
-            "name": "The Third Eye",
-            "cost": "8",
-            "image": "https://i.pinimg.com/originals/5c/1a/47/5c1a47b4697581867bcdce045731f6f9.jpg",
-            "about": "The Third Eye is a video game based on the Indian mythology of Shiva"
-      }
-]
+  {
+    'name': 'Street Combat',
+    'cost': '0.8',
+    'image':
+      'https://images.pushsquare.com/aa5764f1b09ad/best-ps4-fighting-games-playstation-4-1.original.jpg',
+    'about':
+      'Street Combat is a fighting game developed by Lolo and Pimps and published by Capcom for the PlayStation 4 and Microsoft Windows in 2016.'
+  },
+  {
+    'name': 'Immortal',
+    'cost': '1.9',
+    'image':
+      'https://i.insider.com/5cc0fcd74a16754ef749f365?width=600&format=jpeg&auto=webp',
+    'about': 'Immortal is a Japanese street fight video game developed in 2013.'
+  },
+  {
+    'name': 'Sensei',
+    'cost': '0.9',
+    'image':
+      'https://previews.123rf.com/images/jaxper/jaxper1903/jaxper190300007/133058953-samurai-japanese-warrior-in-armor-ready-for-battle.jpg',
+    'about':
+      'Sensei is a combat video game with various fighting levels having stronger contestants with each level.'
+  },
+  {
+    'name': 'Hangaki Gozen',
+    'cost': '1.8',
+    'image':
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtoSVksQHFH9UVDlY5PCamSpWgeLA-lF4JsQ&usqp=CAU',
+    'about': 'Hangaku Gozen is a female warrior game based on the Greek mythology'
+  },
+  {
+    'name': 'Gangs of Nevada',
+    'cost': '3',
+    'image': 'https://i.ytimg.com/vi/YhYctIBKFbM/hqdefault.jpg',
+    'about':
+      'Gangs of Nevada is an American comic based video game with inspiration from American gang history.'
+  },
+  {
+    'name': 'The Third Eye',
+    'cost': '8',
+    'image':
+      'https://i.pinimg.com/originals/5c/1a/47/5c1a47b4697581867bcdce045731f6f9.jpg',
+    'about':
+      'The Third Eye is a video game based on the Indian mythology of Shiva'
+  }
+];
